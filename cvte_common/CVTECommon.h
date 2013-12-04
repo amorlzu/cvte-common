b38c7cd9a8b1715aaa4ddd1f2f7e60f66068c631
tiannan <tiannan@me.com>
2013-12-04 18:58:29 +0800

    [add]common header

---
+++ cvte_common/CVTECommon.h
@@ -0,0 +1,14 @@
+//
+//  CVTECommon.h
+//  cvte_common_demo
+//
+//  Created by Seewo on 13-12-4.
+//  Copyright (c) 2013年 cvte. All rights reserved.
+//
+
+#ifndef cvte_common_demo_CVTECommon_h
+#define cvte_common_demo_CVTECommon_h
+
+
+
+#endif
